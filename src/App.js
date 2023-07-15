0087e16a2ebf4cd52a7451b81a4e5bf9af9abb40
Adam Jami <adamjam39@gmail.com>
2023-07-15 06:53:11 -0400

    e

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import './App.css';
+import Navbar1 from './components/Navbar'
+import Home from './components/Home'
+import About from './components/about';
+import Bot from './components/bot';
+import Booking from './components/booking';
+import Destinations from './components/destinations';
+import { Routes, Route  } from 'react-router-dom';
+import Price from './components/pricing';
+
+
+function App() {
+  const imageUrl = 'https://wallpapercave.com/w/wp6686021.jpg';
+  
+
+  return ( 
+    <div>
+
+      <Navbar1/>
+      <Routes>
+        <Route path="/" element={<Home imageUrl={imageUrl}/>} />
+        <Route path="/home" element={<Home imageUrl={imageUrl}/>} />
+        <Route path="/bot" element={<Bot/>} />
+        <Route path="/about" element={<About/>} />
+        <Route path="/pricing" element={<Price/>} />
+        <Route path="/destination" element={<Destinations/>} />
+        <Route path="/booking" element={<Booking/>} />
+      </Routes>
+      
+
+
+    </div>
+
+    
+  );
+}
+
+export default App;
---
+++ src/components/booking.js
@@ -0,0 +1,17 @@
+import HorizontalLinearStepper from "./Stepper";
+import './bookingstyle.css'
+import Footer from "./footer";
+
+
+function Booking(){
+  return(
+    <div>
+      <div id="bart" className="background-image-container">
+      <HorizontalLinearStepper/>
+    </div>
+    
+    </div>
+  );
+}
+
+export default Booking;
